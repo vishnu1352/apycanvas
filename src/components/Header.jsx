b4416ff478ecafcaff4d92c61b287c84fce77e98
Vishnu vardhan <vishnuvardhan18100@gmail.com>
2024-06-27 01:56:15 +0530

    base url updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({children}) => {
 
   return (
     <div className="headerdiv d-flex justify-content-between align-items-center position-sticky fixed-top">
-      <p className="m-0 p-3 text-center fs-18" onDoubleClick={()=>navigate("/addinventory")}>Customized Keychains</p>
+      <p className="m-0 p-3 text-center fs-18" onDoubleClick={()=>navigate("/addinventory")}>APY Canvas</p>
       {children}
     </div>
   );
